b08d644f7e9af120c560b310d5d920b192cddaa4
TheBloodyAmateur <botan_kadir@outlook.com>
2025-02-06 16:19:06 +0100

    Testing to publish it on maven repo

---
+++ src/main/java/io/github/eventmanager/filehandlers/config/ConfigLogRotate.java
@@ -0,0 +1,35 @@
+package io.github.eventmanager.filehandlers.config;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+
+public class ConfigLogRotate {
+    private AtomicInteger maxSizeInKB = new AtomicInteger(1024);
+    private AtomicInteger rotationPeriodInSeconds = new AtomicInteger(86400);
+    private AtomicReference<String> compressionFormat = new AtomicReference<>("gzip");
+
+    public int getMaxSizeInKB() {
+        return maxSizeInKB.get();
+    }
+
+    public void setMaxSizeInKB(int maxSizeInKB) {
+        this.maxSizeInKB.set(maxSizeInKB);
+    }
+
+    public int getRotationPeriodInSeconds() {
+        return rotationPeriodInSeconds.get();
+    }
+
+    public void setRotationPeriodInSeconds(int rotationPeriodInSeconds) {
+        this.rotationPeriodInSeconds.set(rotationPeriodInSeconds);
+    }
+
+    public String getCompressionFormat() {
+        return compressionFormat.get();
+    }
+
+    public void setCompressionFormat(String compressionFormat) {
+        this.compressionFormat.set(compressionFormat);
+    }
+}
