8bf80fd0a89096e29f211e003196fdaca6758e38
TheBloodyAmateur <botan_kadir@outlook.com>
2025-02-06 14:40:49 +0100

    More testing the new gitignore.

--- src/test/java/dev/github/eventmanager/EventManagerTest.java
+++ src/test/java/dev/github/eventmanager/EventManagerTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class EventManagerTest {
-    String configPath = "confg/loggingConfig.jason";
+    String configPath = "confg/loggingConfig.json";
 
     @Test
     void createInstance() {
