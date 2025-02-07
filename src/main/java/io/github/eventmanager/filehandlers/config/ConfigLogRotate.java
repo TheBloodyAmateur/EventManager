6969fbade595f14c70883ad85c3d7527cbb84541
TheBloodyAmateur <botan_kadir@outlook.com>
2025-02-07 13:37:27 +0100

    Increasing the default value for maxSizeInKB to 10240 (10MB)

--- src/main/java/io/github/eventmanager/filehandlers/config/ConfigLogRotate.java
+++ src/main/java/io/github/eventmanager/filehandlers/config/ConfigLogRotate.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 
 public class ConfigLogRotate {
-    private AtomicInteger maxSizeInKB = new AtomicInteger(1024);
+    private AtomicInteger maxSizeInKB = new AtomicInteger(10240);
     private AtomicInteger rotationPeriodInSeconds = new AtomicInteger(86400);
     private AtomicReference<String> compressionFormat = new AtomicReference<>("gzip");
 
