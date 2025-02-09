db7b37f8b933675b37988d50d9732fbfdf1cd104
TheBloodyAmateur <botan_kadir@outlook.com>
2025-02-09 20:17:42 +0100

    Adding previously ignored package

---
+++ src/main/java/com/github/eventmanager/filehandlers/config/Config.java
@@ -0,0 +1,10 @@
+package com.github.eventmanager.filehandlers.config;
+
+import lombok.Getter;
+
+@Getter
+public class Config {
+    private ConfigEvent event = new ConfigEvent();
+    private ConfigLogFile logFile = new ConfigLogFile();
+    private ConfigLogRotate logRotateConfig = new ConfigLogRotate();
+}
