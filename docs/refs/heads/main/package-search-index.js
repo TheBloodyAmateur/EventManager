115ac47db2f7380f0f7dee1b0c95f60f57ff1b50
TheBloodyAmateur <63422345+TheBloodyAmateur@users.noreply.github.com>
2025-02-13 12:45:39 +0000

    Deploying to gh-pages from @ TheBloodyAmateur/EventManager@bf6f75b37759daec8007db9fa3f5f52283473d33 🚀

---
+++ docs/refs/heads/main/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.eventmanager"},{"l":"com.github.eventmanager.compressors"},{"l":"com.github.eventmanager.filehandlers"},{"l":"com.github.eventmanager.filehandlers.config"},{"l":"com.github.eventmanager.formatters"}];updateSearchResults();
