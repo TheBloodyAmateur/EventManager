a993407c3b157be0b49624f77e35d407667014f6
TheBloodyAmateur <botan_kadir@outlook.com>
2025-02-14 12:06:01 +0100

    release v1.0.28

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,7 @@
+/**
+ * This module defines the event manager application, which includes functionalities for logging events,
+ * handling file operations, and formatting events.
+ */
 module com.github.thebloodyamateur.eventmanager {
     requires java.base;
     requires com.fasterxml.jackson.databind;
