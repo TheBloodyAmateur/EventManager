00fa9e756cc70471a91e968a6ee4b22d29925418
TheBloodyAmateur <botan_kadir@outlook.com>
2025-02-20 21:28:04 +0100

    debugging: modifying the script to produce the index file

--- scripts/generate_index.sh
+++ scripts/generate_index.sh
@@ -66,14 +66,14 @@ echo '<!DOCTYPE html>
     <div class="container">
         <h1>Javadoc Versions</h1>
         <p>Select a version to view:</p>
-        <ul>' > docs/javadoc/index.html
+        <ul>' > gh-pages/javadoc/index.html
 
 # Loop through all versioned Javadoc folders inside docs/javadoc/javadoc/
 for dir in docs/javadoc/*; do
   if [[ -d "$dir" ]]; then
     version=$(basename "$dir")
     echo "Generating index for version $version"
-    echo "<li><a href='$version/index.html'>Version $version</a></li>" >> docs/javadoc/index.html
+    echo "<li><a href='$version/index.html'>Version $version</a></li>" >> gh-pages/javadoc/index.html
   fi
 done
 
@@ -81,4 +81,4 @@ echo '</ul>
     <div class="footer">Documentation hosted on GitHub Pages</div>
     </div>
 </body>
-</html>' >> docs/javadoc/index.html
+</html>' >> gh-pages/javadoc/index.html
