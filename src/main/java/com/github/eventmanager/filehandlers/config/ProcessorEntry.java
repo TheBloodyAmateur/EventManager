5507c2fd8b985abb6a00a55a6fbc28ddde75f689
TheBloodyAmateur <botan_kadir@outlook.com>
2025-03-06 20:27:55 +0100

    docs: add documentation for ProcessorEntry class and its fields

--- src/main/java/com/github/eventmanager/filehandlers/config/ProcessorEntry.java
+++ src/main/java/com/github/eventmanager/filehandlers/config/ProcessorEntry.java
@@ -5,10 +5,20 @@ import lombok.Setter;
 
 import java.util.Map;
 
+/**
+ * The ProcessorEntry class holds the configuration settings for the event processors.
+ * It includes settings for the name of the processor and its parameters.
+ */
 @Setter
 @Getter
 public class ProcessorEntry {
+    /**
+     * The name of the processor(class).
+     */
     private String name;
+    /**
+     * The parameters for the processor.
+     */
     private Map<String, Object> parameters;
 
 }
