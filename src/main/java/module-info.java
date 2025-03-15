13fe81234af5f1b05ebdb294e6ac26b929c8d7c5
TheBloodyAmateur <botan_kadir@outlook.com>
2025-03-15 22:52:46 +0100

    refactor: rename module to improve clarity and consistency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@
  * This module defines the event manager application, which includes functionalities for logging events,
  * handling file operations, and formatting events.
  */
-module com.github.thebloodyamateur.eventmanager {
+module com.github.eventmanager {
     requires java.base;
     requires com.fasterxml.jackson.databind;
     requires static lombok;
